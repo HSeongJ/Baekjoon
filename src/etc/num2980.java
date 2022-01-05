1271a53620cf07f09da9a894f5f4e4d2e3c7bd58
HSeongJ <cal8847@naver.com>
2022-01-05 23:32:29 +0900

    commit

--- src/etc/num2980.java
+++ src/etc/num2980.java
@@ -13,19 +13,38 @@ public class num2980 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		boolean[] L = new boolean[Integer.parseInt(st.nextToken())];
+		int L = Integer.parseInt(st.nextToken());
 		int[][] signal = new int[N][3];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			signal[i][0] = Integer.parseInt(st.nextToken());
 			signal[i][1] = Integer.parseInt(st.nextToken());
 			signal[i][2] = Integer.parseInt(st.nextToken());
+		} //finished for
+
+		int time = 0;
+
+		for(int i = 0; i < N; i++) {
+			if(i == 0) {
+				time += signal[i][0];
+			} else {
+				time += signal[i][0] - signal[i - 1][0];
+			}
+
+			if(time % (signal[i][1] + signal[i][2]) == 0) {
+				time += signal[i][1];
+			} else if(time % (signal[i][1] + signal[i][2]) <= signal[i][1]) {
+				//Red
+				time += signal[i][1] - (time % (signal[i][1] + signal[i][2]));
+			}
 		}
-		
-		
+
+		time += L - (signal[N-1][0]);
+
+		System.out.println(time);
+
 		br.close();
 	}
-
 }
