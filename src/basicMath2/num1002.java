ec51caa1183458ca767bff4bfb92d65c2d3dca62
HSeongJ <cal8847@naver.com>
2022-01-05 21:12:54 +0900

    commit

--- src/basicMath2/num1002.java
+++ src/basicMath2/num1002.java
@@ -6,36 +6,36 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class num1002 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		while(T-- > 0) {
-			String str = br.readLine();
-			
-			String[] str1 = str.split(" ");
-			
-			int[] first = {Integer.parseInt(str1[0]), Integer.parseInt(str1[1]), Integer.parseInt(str1[2])};
-			int[] second = {Integer.parseInt(str1[3]), Integer.parseInt(str1[4]), Integer.parseInt(str1[5])};
-			
-			int distance = (int)(Math.pow(first[0] - second[0], 2) + Math.pow(first[1] - second[1], 2));
-			
-			if(first[0] == second[0] && first[1] == second[1] && first[2] == second[2])
-				bw.write(String.valueOf(-1 + "\n"));
-			else if((distance > Math.pow(first[2] + second[2], 2))
-					|| distance < Math.pow(first[2] - second[2], 2))
-				bw.write(String.valueOf(0 + "\n"));
-			else if((distance == Math.pow(first[2] + second[2], 2))
-					|| distance == Math.pow(first[2] - second[2], 2))
-				bw.write(String.valueOf(1 + "\n"));
-			else
-				bw.write(String.valueOf(2 + "\n"));
-		}
-		br.close();
-		
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            String str = br.readLine();
+
+            String[] str1 = str.split(" ");
+
+            int[] first = {Integer.parseInt(str1[0]), Integer.parseInt(str1[1]), Integer.parseInt(str1[2])};
+            int[] second = {Integer.parseInt(str1[3]), Integer.parseInt(str1[4]), Integer.parseInt(str1[5])};
+
+            int distance = (int) (Math.pow(first[0] - second[0], 2) + Math.pow(first[1] - second[1], 2));
+
+            if (first[0] == second[0] && first[1] == second[1] && first[2] == second[2])
+                bw.write(String.valueOf(-1 + "\n"));
+            else if ((distance > Math.pow(first[2] + second[2], 2))
+                    || distance < Math.pow(first[2] - second[2], 2))
+                bw.write(String.valueOf(0 + "\n"));
+            else if ((distance == Math.pow(first[2] + second[2], 2))
+                    || distance == Math.pow(first[2] - second[2], 2))
+                bw.write(String.valueOf(1 + "\n"));
+            else
+                bw.write(String.valueOf(2 + "\n"));
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
 }
